23b4a85f3ca993435149435d442ee7983a4e826f
Raymond Loman <raymond@carpago.nl>
2018-11-14 11:38:06 +0100

    Add (more) lambda instructions

--- src/nl/smulweb/foodcorner/lambdas/Application.java
+++ src/nl/smulweb/foodcorner/lambdas/Application.java
@@ -2,17 +2,47 @@ package nl.smulweb.foodcorner.lambdas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Application {
 
    public static void main(String[] args) {
-      
-      List<Integer> numberList = Arrays.asList(0,1,1,2,3,5,8,13,21,34,55,89);
-      Predicate <Integer> onsPredicate = new MyEvenNumberTestingPredicate();
-      numberList.stream().filter(onsPredicate).forEach(n -> {
+
+      System.out.println("Even");
+      List<Integer> numberList = Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89);
+
+      System.out.println("Oneven");
+      numberList.stream().filter(n -> n % 2 == 1).forEach(n -> {
          System.out.println(n);
       });
+
+      System.out.println("Groter dan 15");
+      numberList.stream().filter(n -> n > 15).forEach(n -> {
+         System.out.println(n);
+      });
+
+      Predicate<Integer> iPredicate = n -> n > 30;
+
+      Predicate<Integer> langerePredicate = n -> {
+         int m = n * n;
+
+         return m > 25;
+      };
+      
+      BiFunction<Integer, Integer, Integer> eenFunctieMetTweeInEnEenUit = (n, m)-> n*m;
+      
+      Consumer<Integer> etertje = n -> {
+         System.out.println(n);
+      };
+      
+      System.out.println("Kwadraten");
+      numberList.stream().filter(n -> n %2 == 0).map(j -> j * j).forEach(e -> {
+         System.out.println(e);
+      });
+      
+      
 
    }
 
