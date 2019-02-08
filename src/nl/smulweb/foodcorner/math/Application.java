4177d3d3404b5ba529ce05268199a168a733b371
Raymond Loman <raymond@carpago.nl>
2019-02-08 18:30:08 +0100

    ADd code

---
+++ src/nl/smulweb/foodcorner/math/Application.java
@@ -0,0 +1,21 @@
+package nl.smulweb.foodcorner.math;
+
+public class Application {
+
+   public static void main(String[] args) {
+
+      System.out.println(Math.sqrt(2));
+
+      System.out.println(Math.random() * 1000);
+
+      System.out.println(Math.max(15, 45));
+
+      System.out.println(Math.E);
+
+      System.out.println(Math.PI);
+
+      System.out.println(Math.sin(45));
+
+   }
+
+}
