7d55fbac54da5161defe077417b4452411e61d04
Raymond Loman <raymond@carpago.nl>
2019-02-11 14:04:01 +0100

    Add more formatting code

--- src/nl/smulweb/foodcorner/formatting/strings/Application.java
+++ src/nl/smulweb/foodcorner/formatting/strings/Application.java
@@ -1,21 +1,19 @@
 package nl.smulweb.foodcorner.formatting.strings;
 
-
 public class Application {
-   
+
    public static void main(String[] args) {
-      
-      final String[] teamsHome = new String[]{"Groningen", "Almelo", "Feyenoord"};
-      final String[] teamsOut = new String[]{"Vitesse", "Ajax", "Utrecht"};
-    
-      final int[] goalsHome = {2,12,3};
-      final int[] goalsOut = {1,3,4};
-      
-      for(int i = 0;i<teamsHome.length;i++) {
+
+      final String[] teamsHome = new String[] { "Groningen", "Almelo", "Feyenoord" };
+      final String[] teamsOut = new String[] { "Vitesse", "Ajax", "Utrecht" };
+
+      final int[] goalsHome = { 2, 12, 3 };
+      final int[] goalsOut = { 1, 3, 4 };
+
+      for (int i = 0; i < teamsHome.length; i++) {
          System.out.printf(">Game %20s against %-20s has resulted in %02d-%02d  <%n", teamsHome[i], teamsOut[i], goalsHome[i], goalsOut[i]);
       }
-    
-      
+
    }
 
 }
